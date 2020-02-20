c8202adf9d808ec4772c6e46accd83bfdc0f73a0
switchYello <1062641125@qq.com>
2020-02-20 20:09:50 +0800

    再addHandler内开启ping pong

--- frps/src/main/java/com/fys/handler/PingPongHandler.java
+++ frps/src/main/java/com/fys/handler/PingPongHandler.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * 此类只有当server开启成功后才会被添加到pipline
  * hcy 2020/2/20
  * 处理管理连接 ping pong 的 handler
  * 逻辑是定时发送ping，如果长时间没收到pong 则主动断开连接
@@ -19,24 +20,24 @@ public class PingPongHandler extends ChannelInboundHandlerAdapter {
 
     private static Logger log = LoggerFactory.getLogger(PingPongHandler.class);
     //最后一次接收到pong的时间戳
-    private long lastPong = System.currentTimeMillis();
+    private long lastPong;
     //最大多久没有pong就断开，毫秒
-    private long maxPongDelay = 5000 * 4;
-    //多长时间ping一次，单位秒
+    private static long maxPongDelay = 5000 * 4;
+    //多长时间ping一次，秒
     private int pingRate = 5;
-    //是否是接收到的第一个数据
-    private boolean first = true;
 
-    
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        ctx.executor().schedule(new SchedulePing(ctx), 1, TimeUnit.SECONDS);
+        lastPong = System.currentTimeMillis();
+        super.handlerAdded(ctx);
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof Pong) {
             lastPong = System.currentTimeMillis();
             log.info("收到客户端pong");
-            if (first) {
-                first = false;
-                ctx.executor().schedule(new SchedulePing(ctx), 1, TimeUnit.SECONDS);
-            }
         } else {
             super.channelRead(ctx, msg);
         }
