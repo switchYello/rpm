afa62d8ea1e7ae395c8fa50c12e80e7d9a4ad28a
switchYello <1062641125@qq.com>
2020-02-26 16:04:32 +0800

    使用md5算法来代替前面临时的has0h算法

--- common/src/main/java/com/fys/cmd/message/clientToServer/WantManagerCmd.java
+++ common/src/main/java/com/fys/cmd/message/clientToServer/WantManagerCmd.java
@@ -3,6 +3,11 @@ package com.fys.cmd.message.clientToServer;
 import com.fys.cmd.exception.AuthenticationException;
 import com.fys.cmd.message.Cmd;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -15,23 +20,23 @@ public class WantManagerCmd implements Cmd {
     private int serverWorkPort;
     private String localHost;
     private int localPort;
-    private int hash;
+    private byte[] md5;
 
     public WantManagerCmd(int serverWorkPort, String localHost, int localPort, String password) {
         this.serverWorkPort = serverWorkPort;
         this.localHost = localHost;
         this.localPort = localPort;
-        this.hash = (serverWorkPort + localHost + localPort + password).hashCode();
+        this.md5 = md5(serverWorkPort + localHost + localPort + password);
     }
 
     @Override
     public void encoderTo(ByteBuf buf) {
-        buf.writeByte(ClientToServer.wantManagerCmd);
-        buf.writeShort(serverWorkPort);
-        buf.writeShort(localHost.length());
-        buf.writeCharSequence(localHost, UTF_8);
-        buf.writeShort(localPort);
-        buf.writeInt(hash);
+        buf.writeByte(ClientToServer.wantManagerCmd); //标志位
+        buf.writeShort(serverWorkPort);             //服务端口
+        buf.writeShort(localHost.length());         //本地host 长度
+        buf.writeCharSequence(localHost, UTF_8);    //本地host
+        buf.writeShort(localPort);                  //本地端口
+        buf.writeBytes(md5);                        //md5
     }
 
 
@@ -39,8 +44,10 @@ public class WantManagerCmd implements Cmd {
         int serverWorkPort = in.readUnsignedShort();
         CharSequence localHost = in.readCharSequence(in.readUnsignedShort(), UTF_8);
         int localPort = in.readUnsignedShort();
-        int hash = in.readInt();
-        if ((serverWorkPort + localHost.toString() + localPort + password).hashCode() == hash) {
+        ByteBuf md5 = in.readBytes(16);
+
+        byte[] serverMd5 = md5(serverWorkPort + localHost.toString() + localPort + password);
+        if (ByteBufUtil.equals(md5, Unpooled.wrappedBuffer(serverMd5))) {
             return new WantManagerCmd(serverWorkPort, localHost.toString(), localPort, password);
         }
         throw AuthenticationException.INSTANCE;
@@ -69,4 +76,18 @@ public class WantManagerCmd implements Cmd {
                 ", localPort=" + localPort +
                 '}';
     }
+
+    /*
+     * MD5算法摘要出的是128bit的数据，等于16Byte数据，转成16进制字符串为32个字符
+     * */
+    private static byte[] md5(String src) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            return md.digest(src.getBytes(UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return new byte[16];
+    }
+
 }
