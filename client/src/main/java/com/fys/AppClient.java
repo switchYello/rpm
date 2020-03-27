e7a43dac45044b60f2a6dd6b3e9626b66d04c2df
switchYello <1062641125@qq.com>
2020-03-27 14:26:17 +0800

    使用回掉+定时方式实现掉线重连

--- client/src/main/java/com/fys/AppClient.java
+++ client/src/main/java/com/fys/AppClient.java
@@ -24,77 +24,29 @@ public class AppClient {
     private static AttributeKey<Config> key = AttributeKey.newInstance("config");
 
     private Config config = Config.INSTANCE;
-    //管理连接
-    private volatile Channel managerChannel;
-
-    private static class Watch {
-
-        long lastStart = 0;
-        long lastStop = 0;
-        int stopCount = 0;
-        boolean start = false;
-
-        //保存最后启动时间
-        public void start() {
-            lastStart = System.currentTimeMillis();
-            start = true;
-        }
-
-        //保存最后停止时间和总停止次数
-        //如果当前是启动状态，则输出持续时间
-        public void stop() {
-            stopCount++;
-            lastStop = System.currentTimeMillis();
-            if (start) {
-                start = false;
-                log.info("上次坚持:{}分钟,共停止{}次", TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis() - lastStart), stopCount);
-            } else {
-                log.info("上次连接没成功,共停止{}次", stopCount);
-            }
-        }
-
-    }
 
     public static void main(String[] args) {
         AppClient appClient = new AppClient();
-        Watch watch = new Watch();
-        work.scheduleWithFixedDelay(() -> {
-            if (appClient.isActive()) {
-                return;
-            }
-            try {
-                log.info("监测到连接断开，准备重连");
-                watch.stop();
-                appClient.start().addListener((ChannelFutureListener) future -> {
-                    if (future.isSuccess()) {
-                        watch.start();
-                    }
-                });
-            } catch (Exception e) {
-                log.error("重连时报错", e);
-            }
-        }, 0, 10, TimeUnit.SECONDS);
+        appClient.start();
     }
 
-
-    private ChannelFuture start() {
-        return createManagerConnection()
+    
+    private void start() {
+        createManagerConnection()
                 .addListener((ChannelFutureListener) future -> {
                     if (future.isSuccess()) {
                         log.info("连接成功{}:{}等待服务端验证", config.getServerIp(), config.getServerPort());
+                        //断开连接10s后会重试
+                        future.channel().closeFuture().addListener(f -> work.schedule(this::start, 10, TimeUnit.SECONDS));
                         //登录
                         future.channel().writeAndFlush(new LoginCmd(config.getClientName(), config.getToken()));
-                        managerChannel = future.channel();
                     } else {
-                        log.error("连接失败:{}", future.cause().toString());
+                        log.error("连接失败,5秒后重试:{}", future.cause().toString());
+                        work.schedule(this::start, 5, TimeUnit.SECONDS);
                     }
                 });
     }
 
-
-    private boolean isActive() {
-        return managerChannel != null && managerChannel.isActive();
-    }
 
     private ChannelFuture createManagerConnection() {
         Bootstrap b = new Bootstrap();
