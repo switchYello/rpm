bb2b5ee6eb6d608ce7cdf772d7ec824e44bbcc3e
switchYello <1062641125@qq.com>
2020-04-03 18:01:31 +0800

    使用AppClint的静态key对象

--- client/src/main/java/com/fys/handler/DataConnectionHandler.java
+++ client/src/main/java/com/fys/handler/DataConnectionHandler.java
@@ -1,12 +1,12 @@
 package com.fys.handler;
 
+import com.fys.AppClient;
 import com.fys.Config;
 import com.fys.DataConnectionClient;
 import com.fys.cmd.message.DataConnectionCmd;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.Attribute;
-import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +22,7 @@ public class DataConnectionHandler extends SimpleChannelInboundHandler<DataConne
      * */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DataConnectionCmd msg) {
-        Attribute<Config> conf = ctx.channel().attr(AttributeKey.valueOf("config"));
+        Attribute<Config> conf = ctx.channel().attr(AppClient.key);
         Config config = conf.get();
         log.debug("收到服务端DataConnection,{} -> {}:{}", msg.getServerPort(), msg.getLocalHost(), msg.getLocalPort());
         new DataConnectionClient(config, msg).start();
