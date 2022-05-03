c3dcfc6b866fffa2cb6acc28c50066a5d37bbd2d
huangcy_sh <huangcy_sh@ushareit.com>
2022-05-03 20:54:17 +0800

    客户端绑定逻辑变更

--- client/src/main/java/com/fys/connection/DataConnection.java
+++ client/src/main/java/com/fys/connection/DataConnection.java
@@ -39,21 +39,30 @@ public class DataConnection {
         this.msg = msg;
     }
 
+    ConnectionToLocal localConnection;
+    ConnectionToService serviceConnection;
+
+    //1.连接客户端和服务端
     public void startConnection() {
-        ConnectionToLocal local = new ConnectionToLocal();
-        ConnectionToService service = new ConnectionToService();
-        local.bindToService(service);
-        service.bindToLocal(local);
+        localConnection = new ConnectionToLocal();
+        serviceConnection = new ConnectionToService();
+        localConnection.bindToService(serviceConnection);
+    }
+
+    //2.客户端连接成功后，才开始bind服务端
+    private void onClientActive(ConnectionToLocal local) {
+        serviceConnection.bindToLocal(local);
+    }
+
+    //3.服务器连接成功后，发送指令
+    private void onServiceActive(ConnectionToService service) {
+        NewDataConnectionCmd cmd = new NewDataConnectionCmd(msg.getSessionId());
+        service.writeAndFlush(cmd);
     }
 
     private void onClientRead(ByteBuf msg, ConnectionToLocal local, ConnectionToService service) {
         RawDataCmd rawDataCmd = new RawDataCmd(msg);
         service.writeAndFlush(rawDataCmd);
-    }
-
-    private void onServiceActive(ConnectionToService service) {
-        NewDataConnectionCmd cmd = new NewDataConnectionCmd(msg.getSessionId());
-        service.writeAndFlush(cmd);
     }
 
     private void onServiceRead(Object msg, ConnectionToLocal local, ConnectionToService service) {
@@ -81,12 +90,17 @@ public class DataConnection {
             channelToLocal.addListener(new ChannelFutureListener() {
                 @Override
                 public void operationComplete(ChannelFuture future) {
-                    pipeline.addLast(new SimpleChannelInboundHandler<ByteBuf>() {
-                        @Override
-                        protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
-                            onClientRead(msg, ConnectionToLocal.this, service);
-                        }
-                    });
+                    if (future.isSuccess()) {
+                        pipeline.addLast(new SimpleChannelInboundHandler<ByteBuf>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
+                                onClientRead(msg, ConnectionToLocal.this, service);
+                            }
+                        });
+                        onClientActive(ConnectionToLocal.this);
+                    } else {
+                        log.info("连接到local失败 - {}", channel);
+                    }
                 }
             });
         }
@@ -100,6 +114,7 @@ public class DataConnection {
         }
 
         public ChannelFuture close() {
+            channelToLocal.cancel(false);
             return channel.close();
         }
 
@@ -123,24 +138,19 @@ public class DataConnection {
             channelToService.addListener(new ChannelFutureListener() {
                 @Override
                 public void operationComplete(ChannelFuture future) {
-                    pipeline.addLast(new CmdEncoder()); //编码器
-                    pipeline.addLast(new CmdDecoder()); //解码器
-                    pipeline.addLast(new SimpleChannelInboundHandler<Cmd>() {
-
-                        //连接成功后，发送指令
-                        @Override
-                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                            onServiceActive(ConnectionToService.this);
-                        }
-
-                        /**
-                         * 收到服务端StartTransactionCmd时，开始从客户端读数据
-                         */
-                        @Override
-                        protected void channelRead0(ChannelHandlerContext ctx, Cmd msg) {
-                            onServiceRead(msg, local, ConnectionToService.this);
-                        }
-                    });
+                    if (future.isSuccess()) {
+                        pipeline.addLast(new CmdEncoder()); //编码器
+                        pipeline.addLast(new CmdDecoder()); //解码器
+                        pipeline.addLast(new SimpleChannelInboundHandler<Cmd>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, Cmd msg) {
+                                onServiceRead(msg, local, ConnectionToService.this);
+                            }
+                        });
+                        onServiceActive(ConnectionToService.this);
+                    } else {
+                        log.info("连接到服务端失败 - {}", channel);
+                    }
                 }
             });
         }
@@ -150,6 +160,7 @@ public class DataConnection {
         }
 
         public ChannelFuture close() {
+            channelToService.cancel(false);
             return channel.close();
         }
 
